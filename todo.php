172acc4b87d4f545691162f1db8b11b791287dbb
icodebang <icodebang@126.com>
2018-08-08 22:48:23 +0800

    猎取工作列表

---
+++ todo.php
@@ -0,0 +1,41 @@
+<?php
+
+exit();
+
+?>
+
+1. icodebang.com
+    文件注释替换
+    logo
+    抓取文章js
+    首页
+    导航管理
+    网站ACL设置：黑白名单
+    切换成CMS
+    广告管理
+    关键字管理
+    css变更
+    js切换
+    网站爬虫
+    视频查找
+    职位信息抓取
+    相关函数参考，比较
+    devdocs修改
+    公众号
+    小程序
+    framework变更
+
+2. edulong.cn 高考
+    数据库设计
+    数据获取
+    页面设计
+    公众号
+    小程序
+3. mysqlworkbench.cn
+    页面
+4. heidisql.cn
+    页面
+5. sequelpro.cn
+    页面
+6. englishday.cn
+
