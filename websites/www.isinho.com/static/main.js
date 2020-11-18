9eda5037c08e2535d1e267cb99c992dbcc1b57f3
icodebang <icodebang@126.com>
2020-11-18 15:19:15 +0800

    [code] 底部加入标识性标签，供js判断移除多余元素

--- websites/www.isinho.com/static/main.js
+++ websites/www.isinho.com/static/main.js
@@ -630,7 +630,7 @@
 	//	防止手机浏览器在页面后面加入垃圾广告
 	======================================*/
     $(document).scroll(function () {
-        $("#last-one-flag").nextAll().remove();
+        $("#last-one-flag").nextAll(':not(#scrollUp)').remove();
     });
 
 })(jQuery);
